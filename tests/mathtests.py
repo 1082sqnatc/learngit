78cdb984a0ffda430c7ebea84ba1e1d40210c944
Adam Fowler <adamfowleruk@gmail.com>
2019-12-01 11:10:13 +0000

    Fixed #3. Implemented absolute function and tests.

--- tests/mathtests.py
+++ tests/mathtests.py
@@ -9,3 +9,17 @@ class MathTests(unittest.TestCase):
     numberInstance = Number(num)
     self.assertEqual(num,numberInstance.value)
 
+  def test_absolute_positive(self):
+    num = 3
+    numberInstance = Number(num)
+    self.assertGreater(numberInstance.absolute(),0)
+
+  def test_absolute_negative(self):
+    num = -3
+    numberInstance = Number(num)
+    self.assertGreater(numberInstance.absolute(), 0)
+
+  def test_absolute_zero(self):
+    num = 0
+    numberInstance= Number(num)
+    self.assertEqual(numberInstance.absolute(), 0)
